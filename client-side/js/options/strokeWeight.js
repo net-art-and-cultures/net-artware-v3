c57858feb5adefa3a5118b943585aee95140a74e
nbriz <nickbriz@gmail.com>
2022-04-23 11:38:22 -0500

    updated tools/options

---
+++ client-side/js/options/strokeWeight.js
@@ -0,0 +1,26 @@
+window.options.strokeWeight = {
+  name: 'stroke weight',
+  run: function (e) {
+    const ele = document.createElement('div')
+    ele.innerHTML = `
+      <style>
+        .pencil-options {
+          padding: 10px;
+        }
+      </style>
+
+      <div class="pencil-options">
+        <input type="range" min="1" max="100" value="${e.ctx.lineWidth}">
+      </div>
+    `
+
+    const slider = ele.querySelector('input')
+    slider.addEventListener('input', () => {
+      e.ctx.lineCap = 'round'
+      e.ctx.lineJoin = 'round'
+      e.ctx.lineWidth = slider.value
+    })
+
+    return ele
+  }
+}
