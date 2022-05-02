79a3e32723791e696d497f631f8e6f96bbe74291
emris <mbrand5@saic.edu>
2022-05-02 17:27:44 -0500

    Pushing this up... there is an error but I cleaned up the recipe request

--- server-side/rest-api.js
+++ server-side/rest-api.js
@@ -9,70 +9,63 @@ metadata.set('authorization', 'Key d9c28c50aab0499cb6c8c413b3f264d4') // API key
 
 router.use(bodyParser.json())
 
-// Example REST API "end point"
-router.get('/api/random-dog', async (req, res) => {
-  // get a random dog image from https://dog.ceo
-  const json = await axios.get('https://dog.ceo/api/breeds/image/random')
-  // if we got back data...
-  if (json.data.status === 'success') {
-    res.json({ img: json.data.message }) // ...send it back in respsone...
-  } else { // ...if not...
-    res.json({ error: 'oops! sorry no data' }) // ...send back error message.
-  }
-})
-
-const outputs = [] // where food predictions will go
+module.exports = router
 
 router.post('/api/data-url', async (req, res) => {
   // we can get access to the image data sent by the client via: req.body.dataURL
   // the dataURL is all the pixel data from the canvas encoded in "base64"
   // base64 is a very common way of encoding raw data in a string
-  // (instead of base2, aka binary, or base16 aka hexadecimal, etc)
-  console.log(req.body.dataURL)
+  // console.log(req.body.dataURL)
 
   const canvas = req.body.dataURL.split('base64,')[1]
+  const clarafaiData = {
+    // This is the model ID of Food Recognition Model: https://www.clarifai.com/models/ai-food-recognition
+    model_id: 'bd367be194cf45149e75f01d59f77ba7',
+    inputs: [{ data: { image: { base64: canvas } } }]
+  } // image data from canvas encoded in base64
 
-  stub.PostModelOutputs( // copied from: https://github.com/Clarifai/clarifai-nodejs-grpc
-    {
-      // This is the model ID of Food Recognition Model: https://www.clarifai.com/models/ai-food-recognition
-      model_id: 'bd367be194cf45149e75f01d59f77ba7',
-      inputs: [{ data: { image: { base64: canvas } } }] // image data from canvas encoded in base64
-    },
-    metadata,
-    (err, response) => {
-      if (err) {
-        console.log('Error: ' + err)
-        return
-      }
+  function createSpoonURL (ingredients, num, apiKey) {
+    let url = 'https://api.spoonacular.com/recipes/findByIngredients'
+    url += `?ingredients=${ingredients.join(',+')}`
+    url += `&number=${num}`
+    url += `&apiKey=${apiKey}`
+    return url
+  }
 
-      if (response.status.code !== 10000) {
-        console.log('Received failed status: ' + response.status.description + '\n' + response.status.details)
-        return
-      }
-      console.log('Predicted concepts, with confidence values:')
-      for (const c of response.outputs[0].data.concepts) {
-        console.log(c.name + ': ' + c.value)
-        outputs.push(c.name)
-      }
+  async function getRecipies (ingredients, numRecipies) {
+    const apiKey = '756c19d994a3497a82093e3b4bbe91fe' // Spoontacular API key
+    const recURL = createSpoonURL(ingredients, numRecipies, apiKey)
+    console.log(recURL)
+    // I need to only add a couple of ingredients to the request url
+    const json = await axios.get(recURL)
+    return json // return recipies
+  }
+
+  async function getIngredients (err, response) {
+    if (err) {
+      console.log('Error: ' + err)
+      res.json({ staus: 'error', error: err })
+      return
     }
-  )
+    if (response.status.code !== 10000) {
+      const message = ('Received failed status: ' + response.status.description + '\n' + response.status.details)
+      console.log(message)
+      res.json({ staus: 'error', error: message })
+      return
+    }
+    console.log('Predicted concepts, with confidence values:')
+    const outputs = [] // where food predictions will go
+    for (const c of response.outputs[0].data.concepts) {
+      console.log(c.name + ': ' + c.value)
+      if (c.value > 0.8) { outputs.push(c.name) }
+    }
+    console.log(outputs)
 
-  // ...and eventually we would send some data back to the client
-  // ...like this...
-  res.json({ status: 'success' })
+    // then get recipies
+    const rec = await getRecipies(outputs, 2)
+    // console.log(json)
+    res.json({ status: 'success', data: rec })
+  }
+
+  stub.PostModelOutputs(clarafaiData, metadata, getIngredients)// https://github.com/Clarifai/clarifai-nodejs-grpc
 })
-
-// search for recipies with spoontacular API
-
-let reqURL = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='
-console.log(outputs)
-for (const x of outputs) {
-  const newReq = reqURL + outputs[x] + '},'
-  reqURL = newReq
-}
-console.log(reqURL)
-reqURL = reqURL + '&number=2&apiKey=756c19d994a3497a82093e3b4bbe91fe'
-
-axios.get(reqURL) // is this the right way to do it????
-
-module.exports = router
