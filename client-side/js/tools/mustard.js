3233c7ea1ee4317f02c23c23933f3b11df6897aa
Guang <guang1177@gmail.com>
2022-05-10 23:39:43 -0500

    modified mustard brush

--- client-side/js/tools/mustard.js
+++ client-side/js/tools/mustard.js
@@ -18,9 +18,9 @@ window.tools.mustard = {
     mousemove: function (e) {
       if (e.state.selected && e.state.mousePressed) {
         for (let i = 0; i < e.state.drops; i++) {
-          const radius = Math.random() * 5
-          const x = e.mouse.x + (Math.random() * 10 - 5)
-          const y = e.mouse.y + (Math.random() * 20 - 10)
+          const radius = Math.random() * 5 + 3
+          const x = e.mouse.x + (Math.random() * 15 - 10)
+          const y = e.mouse.y + (Math.random() * 15 - 8)
           e.ctx.beginPath()
           e.ctx.arc(x, y - 10, radius, radius - 20, Math.PI, 0, Math.PI / 2)
           e.ctx.fillStyle = 'rgba(219, 162, 4, 0.2)'
